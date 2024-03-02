3461ad741ee33f44c711d0fa5ea5a16a7e9ee5e6
SopitasWick <jesus.izaguirre216660@potros.itson.edu.mx>
2024-03-02 11:35:24 -0700

    Primer commit

---
+++ app/src/main/java/mx/edu/itson/practica06/Pelicula.kt
@@ -0,0 +1,6 @@
+package mx.edu.itson.practica06
+
+data class Pelicula(var titulo:String,
+                    var image: Int,
+                    var header: Int,
+                    var sinopsis: String)
